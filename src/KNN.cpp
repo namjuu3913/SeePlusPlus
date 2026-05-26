df371a99f0b237445c327cb4eed419751489bc63
mark <09longtr09@gmail.com>
2026-05-26 00:31:24 +0000

    first commit for KNN

--- src/KNN.cpp
+++ src/KNN.cpp
@@ -1,1 +1,31 @@
-//temp
+#include <iostream>
+#include <cmath>
+#include <cfloat>
+#include <cstdint>
+#include <immintrin.h>
+
+const int SIZE = 15;
+
+int findSmallestErr(uint8_t* median, uint8_t* arr[15], int resolution){
+  int smallesIndex;
+  double smallestErr = 1e18;
+  uint8_t *m = median;
+  uint8_t *curr = arr[0];
+  double L1 = 0;
+  for(int i = 0; i<SIZE*resolution; i++){
+    if(i!=0&&i%5==0){
+      if(smallestErr>L1){
+        smallestErr = L1;
+        smallesIndex = i/5;
+      }
+      L1 = 0;
+      m = median;
+      curr = arr[i/5];
+    }
+    L1+=*m-*curr;
+
+    m++;
+    curr++;
+  }
+  return smallesIndex;
+}
